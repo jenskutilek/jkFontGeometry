d55ae79ce55cee7a601d984086cb8196c826bbd1
Jens Kutilek <webmail@kutilek.de>
2019-11-28 16:31:31 +0100

    Minor

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from distutils.core import setup, Extension
 
 setup(
-		name = "jkFontGeometry",
-		version = "0.1",
-		description = "Font geometry tools.",
-		author = "Jens Kutilek",
-		url = "http://www.kutilek.de/",
-		packages = [
-			"jkFontGeometry",
-		],
-		package_dir = {
-			"": "Lib"
-		},
-		ext_modules = [
-			Extension(
-				"jkFontGeometry._fastgeometry", [
-					"Lib/jkFontGeometry/_fastgeometry.cpp",
-					"Lib/jkFontGeometry/fastgeometry.cpp"
-				]
-			)
-		],
-	)
+    name="jkFontGeometry",
+    version="0.1",
+    description="Font geometry tools.",
+    author="Jens Kutilek",
+    url="http://www.kutilek.de/",
+    packages=[
+        "jkFontGeometry",
+    ],
+    package_dir={
+        "": "Lib"
+    },
+    ext_modules=[
+        Extension(
+            "jkFontGeometry._fastgeometry", [
+                "Lib/jkFontGeometry/_fastgeometry.cpp",
+                "Lib/jkFontGeometry/fastgeometry.cpp"
+            ]
+        )
+    ],
+)
